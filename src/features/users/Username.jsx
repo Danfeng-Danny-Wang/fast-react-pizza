46b21d224bcdb2dfcbb929b862ffca2ffce9968d
Danny W <dannywang@utexas.edu>
2023-11-16 23:50:23 -0500

    add styles to forms and buttons

--- src/features/users/Username.jsx
+++ src/features/users/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Danny</div>;
+  return <div className="hidden text-sm font-semibold md:block">Danny</div>;
 }
 
 export default Username;
